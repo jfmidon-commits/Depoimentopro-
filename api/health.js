3030ee7cbb1f8c7306a003897ee22bf455dd3aad
Juliano Midon <jfmidon@gmail.com>
2026-08-30 12:07:55 -0300

    billing: expose safe readiness state in health

--- api/health.js
+++ api/health.js
@@ -2,6 +2,7 @@ const { requireAirtable, requireSessionSecret } = require('../lib/config');
 const { json, method, ensureRequestId } = require('../lib/http');
 const { rateLimitBackend } = require('../lib/rate-limit');
 const { isEnabled: turnstileEnabled } = require('../lib/turnstile');
+const { billingState } = require('../lib/stripe');
 const { logError } = require('../lib/logger');
 
 module.exports = async (req, res) => {
@@ -19,6 +20,7 @@ module.exports = async (req, res) => {
     return json(res, 503, { ok: false, service: 'depoimentopro', version });
   }
 
+  const billing = billingState();
   return json(res, 200, {
     ok: true,
     service: 'depoimentopro',
@@ -26,6 +28,12 @@ module.exports = async (req, res) => {
     protections: {
       rateLimit: rateLimitBackend(),
       turnstile: turnstileEnabled(),
+    },
+    billing: {
+      mode: billing.mode,
+      checkoutConfigured: billing.checkoutConfigured,
+      webhookConfigured: billing.webhookConfigured,
+      liveEnabled: billing.mode === 'live' ? billing.liveEnabled : false,
     },
   });
 };
